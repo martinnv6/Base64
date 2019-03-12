7b8ad733bfa898a5af8d06e465021a8730b1b0ab
Günther Foidl <gue@korporal.at>
2019-03-12 17:12:01 +0100

    API defined
    As extension-method per https://github.com/gfoidl/Base64/issues/61#issuecomment-469157437

---
+++ source/gfoidl.Base64/Extensions/ReadOnlySequenceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Buffers;
+
+namespace gfoidl.Base64
+{
+    public static class ReadOnlySequenceExtensions
+    {
+        public static OperationStatus Encode(this IBase64 encoder, in ReadOnlySequence<byte> data, IBufferWriter<byte> base64, out long consumed, out long written)
+        {
+            throw new NotImplementedException();
+        }
+        //---------------------------------------------------------------------
+        public static OperationStatus Decode(this IBase64 encoder, in ReadOnlySequence<byte> base64, IBufferWriter<byte> data, out long consumed, out long written)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
